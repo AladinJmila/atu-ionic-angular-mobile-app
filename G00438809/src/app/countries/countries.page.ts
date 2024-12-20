0d2a8659b21d9541ce0e719d385e83a9e7520943
Alaeddine Jmila <jmila.aladin@gmail.com>
2024-12-20 17:41:42 +0000

    Load countries from API

--- G00438809/src/app/countries/countries.page.ts
+++ G00438809/src/app/countries/countries.page.ts
@@ -1,20 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
+import { HttpService } from '../services/http.service';
 
 @Component({
   selector: 'app-countries',
   templateUrl: './countries.page.html',
   styleUrls: ['./countries.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+  ],
 })
 export class CountriesPage implements OnInit {
+  countries: any[] = [];
+  constructor(private httpService: HttpService) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  async ngOnInit() {
+    this.getCountries();
   }
 
+  async getCountries() {
+    const { data } = await this.httpService.get({
+      url: 'https://restcountries.com/v3.1/name/ireland',
+    });
+    this.countries = data;
+    console.log(this.countries);
+  }
 }
