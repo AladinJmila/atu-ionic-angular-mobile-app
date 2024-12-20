87c4b3992d6505923d6b28fcbee1c33800e763d5
Alaeddine Jmila <jmila.aladin@gmail.com>
2024-12-20 11:29:33 +0000

    Edit header & add settings icon

--- G00438809/src/app/home/home.page.ts
+++ G00438809/src/app/home/home.page.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonIcon,
+  IonGrid,
+  IonRow,
+  IonCol,
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { settingsOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonIcon,
+    IonGrid,
+    IonRow,
+    IonCol,
+  ],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {
+    addIcons({ settingsOutline });
+  }
 }
