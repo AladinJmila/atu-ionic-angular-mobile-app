ce80e563aa063bcb674b81a1919dd84f802e4d6c
Alaeddine Jmila <jmila.aladin@gmail.com>
2024-12-20 18:05:41 +0000

    Add back button to news

--- G00438809/src/app/news/news.page.ts
+++ G00438809/src/app/news/news.page.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 import {
   IonContent,
   IonHeader,
   IonTitle,
   IonToolbar,
+  IonGrid,
+  IonIcon,
+  IonRow,
+  IonCol,
 } from '@ionic/angular/standalone';
 import { HttpService } from '../services/http.service';
+import { addIcons } from 'ionicons';
+import { chevronBackOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-news',
@@ -15,17 +22,24 @@ import { HttpService } from '../services/http.service';
   styleUrls: ['./news.page.scss'],
   standalone: true,
   imports: [
+    CommonModule,
+    FormsModule,
+    RouterLink,
     IonContent,
     IonHeader,
     IonTitle,
     IonToolbar,
-    CommonModule,
-    FormsModule,
+    IonGrid,
+    IonIcon,
+    IonRow,
+    IonCol,
   ],
 })
 export class NewsPage implements OnInit {
   news: any[] = [];
-  constructor(private httpService: HttpService) {}
+  constructor(private httpService: HttpService) {
+    addIcons({ chevronBackOutline });
+  }
 
   async ngOnInit() {
     this.getCountries();
